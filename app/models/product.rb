77429120515c8d2f924ef8c608e79f5fe9a87f27
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-11-20 12:50:30 -0500

    Add uniqueness constraint to name in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,10 +8,12 @@ class Product
   field :height, type: Integer
   field :weight, type: Integer
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :type, presence: true
   validates :length, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :width, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :height, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :weight, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  index({ name: 1 }, { unique: true })
 end

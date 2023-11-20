f79df9e4f56c3a001fcc2c7c66db2ac612668104
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-11-20 12:11:54 -0500

    Flesh out controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,15 +1,50 @@
 module Api
   module V1
     class ProductsController < ApplicationController
-      def index; end
+      before_action :set_product, only: [:show, :update, :destroy]
 
-      def show; end
+      def index
+        @products = Product.all
+        render json: @products
+      end
 
-      def create; end
+      def show
+        render json: @product
+      end
 
-      def update; end
+      def create
+        @product = Product.new(product_params)
+        if @product.save
+          render json: @product, status: :created, location: @product
+        else
+          render json: @product.errors, status: :unprocessable_entity
+        end
+      end
 
-      def destroy; end
+      def update
+        if @product.update(product_params)
+          render json: @product
+        else
+          render json: @product.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @product.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_product
+        @product = Product.find(params[:id])
+      rescue Mongoid::Errors::DocumentNotFound
+        render json: { error: 'Product not found' }, status: :not_found
+      end
+
+      def product_params
+        params.require(:product).permit(:name, :type, :length, :width, :height, :weight)
+      end
     end
   end
 end

373ceb77dd9709afeb6a53303687b658bc8f65ea
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-11-21 23:59:26 -0500

    refactor product type model spec

--- spec/models/product_type_spec.rb
+++ spec/models/product_type_spec.rb
@@ -11,8 +11,15 @@ RSpec.describe ProductType, type: :model do
   end
 
   describe 'validations' do
+    let(:duplicate_product_type) { ProductType.new(name: 'UniqueName') }
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_uniqueness_of(:name) }
+
+    before { ProductType.create!(name: 'UniqueName') }
+
+    it 'validates uniqueness of name' do
+      expect(duplicate_product_type.valid?).to be false
+      expect(duplicate_product_type.errors[:name]).to include('has already been taken')
+    end
   end
 
   it 'has a unique index on name' do
@@ -20,10 +27,10 @@ RSpec.describe ProductType, type: :model do
   end
 
   describe 'associations' do
-    it 'has many products' do
-      product_type = ProductType.new(name: 'Example Type')
-      product = Product.new(name: 'Example Product', length: 10, width: 10, height: 10, weight: 10, product_type: product_type)
+    let(:product_type) { ProductType.new(name: 'Example Type') }
+    let(:product) { Product.new(name: 'Example Product', length: 10, width: 10, height: 10, weight: 10, product_type: product_type) }
 
+    it 'has many products' do
       expect(product_type.products).to include(product)
     end
   end
